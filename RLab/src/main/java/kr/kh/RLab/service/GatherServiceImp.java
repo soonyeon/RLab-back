16b25c64f51d88db521ea5fcb9169264d06484c5
dohyeon <koolove00221177@gmail.com>
2023-03-29 21:43:30 +0900

    [add] - 임시저장 버튼 눌렀을 때 script추가

--- RLab/src/main/java/kr/kh/RLab/service/GatherServiceImp.java
+++ RLab/src/main/java/kr/kh/RLab/service/GatherServiceImp.java
@@ -13,65 +13,58 @@ import kr.kh.RLab.vo.TagRegisterVO;
 import kr.kh.RLab.vo.TagVO;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor 
+@RequiredArgsConstructor
 @Service
-public class GatherServiceImp implements GatherService{
+public class GatherServiceImp implements GatherService {
 
-	private final GatherDAO gatherDao; 
+	private final GatherDAO gatherDao;
 	String uploadPath = "D:\\uploadfiles";
-	
-	@Override 
-	public boolean insertStudy(StudyVO study, MemberVO member,RegionVO region,MultipartFile [] files,FileVO file,TagVO tag,TagRegisterVO tagRegister) {
-		System.out.println(study);
-		System.out.println(member);
-		System.out.println(files);
-		
-	    if (member == null) {
-	        return false;
-	    }
-	    if (study.getSt_name().trim().length()==0 || study.getSt_total_people() < 1 ) {
-	        return false;
-	    }
-	    study.setSt_me_id(member.getMe_id()); 
-	    gatherDao.insertStudy(study); 
-	    
-	    String[] tags = tag.getTa_name().split(",");
-	    
-	    //각 태그 DB에 저장
-	    for (String tagName : tags) {
-	        TagVO tagVO = new TagVO();
-	        tagVO.setTa_name(tagName.trim());
-	        gatherDao.insertTag(tagVO);
-	        gatherDao.insertStudyTag(study.getSt_num(), tagVO.getTa_name());
-	    }
-	    uploadFiles(files,study.getSt_num(),file.getFi_table());
+
+	@Override
+	public boolean insertStudy(StudyVO study, MemberVO member, RegionVO region, MultipartFile[] files, FileVO file,
+			TagVO tag, TagRegisterVO tagRegister) {
+		if (member == null) {
+			return false;
+		}
+		if (study.getSt_name().trim().length() == 0 || study.getSt_total_people() < 1) {
+			return false;
+		}
+		study.setSt_me_id(member.getMe_id());
+		gatherDao.insertStudy(study);
+
+		String[] tags = tag.getTa_name().split(",");
+
+		// 각 태그 DB에 저장
+		for (String tagName : tags) {
+			TagVO tagVO = new TagVO();
+			tagVO.setTa_name(tagName.trim());
+			gatherDao.insertTag(tagVO);
+			gatherDao.insertStudyTag(study.getSt_num(), tagVO.getTa_name());
+		}
+		uploadFiles(files, study.getSt_num(), file.getFi_table());
 		return true;
 	}
-	
-	private void uploadFiles(MultipartFile [] files, int st_num,String fi_table) {
-		if(files == null || files.length == 0)
-			return ;
-		//반복문
-		for(MultipartFile file : files) {
-			if(file == null || file.getOriginalFilename().length() == 0)
+
+	private void uploadFiles(MultipartFile[] files, int st_num, String fi_table) {
+		if (files == null || files.length == 0)
+			return;
+		// 반복문
+		for (MultipartFile file : files) {
+			if (file == null || file.getOriginalFilename().length() == 0)
 				continue;
 			String fileName = "";
-			//첨부파일 서버에 업로드
+			// 첨부파일 서버에 업로드
 			try {
-				fileName = UploadFileUtils.uploadFile(uploadPath, 
-						file.getOriginalFilename(), 
-						file.getBytes()); 
+				fileName = UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes());
 			} catch (Exception e) {
 				e.printStackTrace();
-			} 
-			//첨부파일 객체를 생성
-			FileVO fileVo = new FileVO(file.getOriginalFilename(), fileName,st_num);
+			}
+			// 첨부파일 객체를 생성
+			FileVO fileVo = new FileVO(file.getOriginalFilename(), fileName, st_num);
 			fileVo.setFi_table(fi_table);
-			//다오에게서 첨부파일 정보를 주면서 추가하라고 요청
+			// 다오에게서 첨부파일 정보를 주면서 추가하라고 요청
 			gatherDao.insertFile(fileVo);
 		}
 	}
-    
 
-		
 }
